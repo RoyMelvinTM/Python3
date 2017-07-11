078eec2ed44183850c06094bd1ee0284c9e751d7
roy <roymelvin21@gmail.com>
2017-07-11 12:19:29 -0500

    cambios

--- listas.py
+++ listas.py
@@ -2,4 +2,5 @@
 my_list = ["Strings", 11, 12.2, True]
 
 my_list.append(6)
+
 print(my_list)
--- tuplas.py
+++ tuplas.py
@@ -1,2 +1,5 @@
+#las tuplas almacenas diferentes tipos de datos y son
+# inmutables ... no se puede modificar (agregar y quitar)
+#se utilizan parentesis a diferencia de listas
 my_tuple = (1, "string", True)
 print (my_tuple[0])
